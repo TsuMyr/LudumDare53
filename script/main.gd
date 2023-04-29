9adf55599fb689ea5ca25004f53e2337a2cdf6b8
SkaOArt <skaoarts@gmail.com>
2023-04-29 13:50:45 +0200

    Sript fürs menu

---
+++ script/main.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var settings = $settings
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_settings_pressed():
+	settings.show()
+
+func _on_credit_pressed():
+	get_tree().change_scene_to_file("res://scenes/credit.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
